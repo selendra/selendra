8c8c2f4af67408a224e18502e73f311bb63a4c8f
LayNath242 <nath.selendra@gmail.org>
2022-06-16 15:10:43 +0700

    add dex modules

--- primitives/src/lib.rs
+++ primitives/src/lib.rs
@@ -36,6 +36,9 @@ use sp_runtime::{
 	FixedU128
 };
 
+#[cfg(feature = "std")]
+use serde::{Deserialize, Serialize};
+
 /// An index to a block.
 pub type BlockNumber = u32;
 
@@ -80,6 +83,48 @@ pub type Amount = i128;
 
 /// Fee multiplier.
 pub type Multiplier = FixedU128;
+
+#[derive(Encode, Eq, PartialEq, Copy, Clone, RuntimeDebug, PartialOrd, Ord, TypeInfo, MaxEncodedLen)]
+#[cfg_attr(feature = "std", derive(Serialize, Deserialize))]
+pub struct TradingPair(CurrencyId, CurrencyId);
+
+impl TradingPair {
+	pub fn from_currency_ids(currency_id_a: CurrencyId, currency_id_b: CurrencyId) -> Option<Self> {
+		if currency_id_a.is_trading_pair_currency_id()
+			&& currency_id_b.is_trading_pair_currency_id()
+			&& currency_id_a != currency_id_b
+		{
+			if currency_id_a > currency_id_b {
+				Some(TradingPair(currency_id_b, currency_id_a))
+			} else {
+				Some(TradingPair(currency_id_a, currency_id_b))
+			}
+		} else {
+			None
+		}
+	}
+
+	pub fn first(&self) -> CurrencyId {
+		self.0
+	}
+
+	pub fn second(&self) -> CurrencyId {
+		self.1
+	}
+
+	pub fn dex_share_currency_id(&self) -> CurrencyId {
+		CurrencyId::join_dex_share_currency_id(self.first(), self.second())
+			.expect("shouldn't be invalid! guaranteed by construction")
+	}
+}
+
+impl Decode for TradingPair {
+	fn decode<I: codec::Input>(input: &mut I) -> sp_std::result::Result<Self, codec::Error> {
+		let (first, second): (CurrencyId, CurrencyId) = Decode::decode(input)?;
+		TradingPair::from_currency_ids(first, second).ok_or_else(|| codec::Error::from("invalid currency id"))
+	}
+}
+
 
 #[derive(Encode, Decode, Eq, PartialEq, Copy, Clone, RuntimeDebug, PartialOrd, Ord, MaxEncodedLen, TypeInfo)]
 #[repr(u8)]
