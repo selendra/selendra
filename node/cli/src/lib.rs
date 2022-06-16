7845a691755979174f7b19d061e366c0c0c88daf
LayNath242 <nath.selendra@gmail.org>
2022-06-16 16:41:24 +0700

    update header

--- node/cli/src/lib.rs
+++ node/cli/src/lib.rs
@@ -1,22 +1,20 @@
-// This file is part of Substrate.
+// Copyright 2021-2022 Selendra.
+// This file is part of Selendra.
 
-// Copyright (C) 2018-2022 Parity Technologies (UK) Ltd.
-// SPDX-License-Identifier: GPL-3.0-or-later WITH Classpath-exception-2.0
-
-// This program is free software: you can redistribute it and/or modify
+// Selendra is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
 
-// This program is distributed in the hope that it will be useful,
+// Selendra is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
 
 // You should have received a copy of the GNU General Public License
-// along with this program. If not, see <https://www.gnu.org/licenses/>.
+// along with Selendra.  If not, see <http://www.gnu.org/licenses/>.
 
-//! Substrate CLI library.
+//! Selendra CLI library.
 //!
 //! This package has two Cargo features:
 //!
