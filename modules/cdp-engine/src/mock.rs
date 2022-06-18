db984ceb7410dd2ec81530dc53a201114bfa11ef
LayNath242 <nath.selendra@gmail.org>
2022-06-18 08:09:21 +0700

    add cdp engine module

---
+++ modules/cdp-engine/src/mock.rs
@@ -0,0 +1,373 @@
+// Copyright 2021-2022 Selendra.
+// This file is part of Selendra.
+
+// Selendra is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// Selendra is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with Selendra.  If not, see <http://www.gnu.org/licenses/>.
+
+//! Mocks for the cdp engine module.
+
+#![cfg(test)]
+
+use super::*;
+use frame_support::{
+	construct_runtime, ord_parameter_types, parameter_types,
+	traits::{ConstU128, ConstU32, ConstU64, Everything, Nothing},
+	PalletId,
+};
+use frame_system::EnsureSignedBy;
+use orml_traits::parameter_type_with_key;
+use primitives::{currency::{DexShare,TokenSymbol}, Moment, TradingPair};
+use sp_core::H256;
+use sp_runtime::{
+	testing::{Header, TestXt},
+	traits::{AccountIdConversion, IdentityLookup, One as OneT},
+};
+use sp_std::cell::RefCell;
+use support::mocks::MockStableAsset;
+use support::{EmergencyShutdown, SpecificJointsSwap};
+
+pub type AccountId = u128;
+pub type BlockNumber = u64;
+
+pub const ALICE: AccountId = 1;
+pub const BOB: AccountId = 2;
+pub const CAROL: AccountId = 3;
+pub const SEL: CurrencyId = CurrencyId::Token(TokenSymbol::SEL);
+pub const SUSD: CurrencyId = CurrencyId::Token(TokenSymbol::SUSD);
+pub const BTC: CurrencyId = CurrencyId::Token(TokenSymbol::RENBTC);
+pub const DOT: CurrencyId = CurrencyId::Token(TokenSymbol::DOT);
+pub const LP_SUSD_DOT: CurrencyId =
+	CurrencyId::DexShare(DexShare::Token(TokenSymbol::SUSD), DexShare::Token(TokenSymbol::DOT));
+pub const LP_DOT_BTC: CurrencyId =
+	CurrencyId::DexShare(DexShare::Token(TokenSymbol::RENBTC), DexShare::Token(TokenSymbol::DOT));
+
+mod cdp_engine {
+	pub use super::super::*;
+}
+
+impl frame_system::Config for Runtime {
+	type Origin = Origin;
+	type Index = u64;
+	type BlockNumber = BlockNumber;
+	type Call = Call;
+	type Hash = H256;
+	type Hashing = ::sp_runtime::traits::BlakeTwo256;
+	type AccountId = AccountId;
+	type Lookup = IdentityLookup<Self::AccountId>;
+	type Header = Header;
+	type Event = Event;
+	type BlockHashCount = ConstU64<250>;
+	type BlockWeights = ();
+	type BlockLength = ();
+	type Version = ();
+	type PalletInfo = PalletInfo;
+	type AccountData = pallet_balances::AccountData<Balance>;
+	type OnNewAccount = ();
+	type OnKilledAccount = ();
+	type DbWeight = ();
+	type BaseCallFilter = Everything;
+	type SystemWeightInfo = ();
+	type SS58Prefix = ();
+	type OnSetCode = ();
+	type MaxConsumers = ConstU32<16>;
+}
+
+parameter_type_with_key! {
+	pub ExistentialDeposits: |_currency_id: CurrencyId| -> Balance {
+		Default::default()
+	};
+}
+
+impl orml_tokens::Config for Runtime {
+	type Event = Event;
+	type Balance = Balance;
+	type Amount = Amount;
+	type CurrencyId = CurrencyId;
+	type WeightInfo = ();
+	type ExistentialDeposits = ExistentialDeposits;
+	type OnDust = ();
+	type MaxLocks = ();
+	type MaxReserves = ();
+	type ReserveIdentifier = [u8; 8];
+	type DustRemovalWhitelist = Nothing;
+	type OnNewTokenAccount = ();
+	type OnKilledTokenAccount = ();
+}
+
+impl pallet_balances::Config for Runtime {
+	type Balance = Balance;
+	type DustRemoval = ();
+	type Event = Event;
+	type ExistentialDeposit = ConstU128<1>;
+	type AccountStore = frame_system::Pallet<Runtime>;
+	type MaxLocks = ();
+	type MaxReserves = ();
+	type ReserveIdentifier = [u8; 8];
+	type WeightInfo = ();
+}
+pub type AdaptedBasicCurrency = orml_currencies::BasicCurrencyAdapter<Runtime, PalletBalances, Amount, BlockNumber>;
+
+parameter_types! {
+	pub const GetNativeCurrencyId: CurrencyId = SEL;
+}
+
+impl orml_currencies::Config for Runtime {
+	type MultiCurrency = Tokens;
+	type NativeCurrency = AdaptedBasicCurrency;
+	type GetNativeCurrencyId = GetNativeCurrencyId;
+	type WeightInfo = ();
+}
+
+parameter_types! {
+	pub const LoansPalletId: PalletId = PalletId(*b"sel/loan");
+}
+
+impl loans::Config for Runtime {
+	type Event = Event;
+	type Currency = Currencies;
+	type RiskManager = CDPEngineModule;
+	type CDPTreasury = CDPTreasuryModule;
+	type PalletId = LoansPalletId;
+	type OnUpdateLoan = ();
+}
+
+thread_local! {
+	static BTC_PRICE: RefCell<Option<Price>> = RefCell::new(Some(Price::one()));
+	static DOT_PRICE: RefCell<Option<Price>> = RefCell::new(Some(Price::one()));
+	static LP_SUSD_DOT_PRICE: RefCell<Option<Price>> = RefCell::new(Some(Price::one()));
+	static LP_DOT_BTC_PRICE: RefCell<Option<Price>> = RefCell::new(Some(Price::one()));
+}
+
+pub struct MockPriceSource;
+impl MockPriceSource {
+	pub fn set_price(currency_id: CurrencyId, price: Option<Price>) {
+		match currency_id {
+			BTC => BTC_PRICE.with(|v| *v.borrow_mut() = price),
+			DOT => DOT_PRICE.with(|v| *v.borrow_mut() = price),
+			LP_SUSD_DOT => LP_SUSD_DOT_PRICE.with(|v| *v.borrow_mut() = price),
+			LP_DOT_BTC => LP_DOT_BTC_PRICE.with(|v| *v.borrow_mut() = price),
+			_ => {}
+		}
+	}
+}
+impl PriceProvider<CurrencyId> for MockPriceSource {
+	fn get_price(currency_id: CurrencyId) -> Option<Price> {
+		match currency_id {
+			BTC => BTC_PRICE.with(|v| *v.borrow()),
+			DOT => DOT_PRICE.with(|v| *v.borrow()),
+			SUSD => Some(Price::one()),
+			LP_SUSD_DOT => LP_SUSD_DOT_PRICE.with(|v| *v.borrow()),
+			LP_DOT_BTC => LP_DOT_BTC_PRICE.with(|v| *v.borrow()),
+			_ => None,
+		}
+	}
+}
+
+parameter_types! {
+	pub const GetStableCurrencyId: CurrencyId = SUSD;
+	pub const CDPTreasuryPalletId: PalletId = PalletId(*b"sel/cdpt");
+	pub TreasuryAccount: AccountId = PalletId(*b"sel/hztr").into_account_truncating();
+	pub AlternativeSwapPathJointList: Vec<Vec<CurrencyId>> = vec![
+		vec![SEL],
+	];
+}
+
+impl cdp_treasury::Config for Runtime {
+	type Event = Event;
+	type Currency = Currencies;
+	type GetStableCurrencyId = GetStableCurrencyId;
+	type UpdateOrigin = EnsureSignedBy<One, AccountId>;
+	type DEX = DEXModule;
+	type Swap = SpecificJointsSwap<DEXModule, AlternativeSwapPathJointList>;
+	type PalletId = CDPTreasuryPalletId;
+	type TreasuryAccount = TreasuryAccount;
+	type WeightInfo = ();
+	type StableAsset = MockStableAsset<CurrencyId, Balance, AccountId, BlockNumber>;
+}
+
+parameter_types! {
+	pub const DEXPalletId: PalletId = PalletId(*b"sel/dexm");
+	pub const GetExchangeFee: (u32, u32) = (0, 100);
+	pub EnabledTradingPairs: Vec<TradingPair> = vec![
+		TradingPair::from_currency_ids(SUSD, BTC).unwrap(),
+		TradingPair::from_currency_ids(SUSD, DOT).unwrap(),
+		TradingPair::from_currency_ids(SEL, BTC).unwrap(),
+		TradingPair::from_currency_ids(SEL, DOT).unwrap(),
+		TradingPair::from_currency_ids(SEL, SUSD).unwrap(),
+	];
+}
+
+impl dex::Config for Runtime {
+	type Event = Event;
+	type Currency = Currencies;
+	type GetExchangeFee = GetExchangeFee;
+	type TradingPathLimit = ConstU32<4>;
+	type PalletId = DEXPalletId;
+	type Erc20InfoMapping = ();
+	type DEXIncentives = ();
+	type WeightInfo = ();
+	type ListingOrigin = EnsureSignedBy<One, AccountId>;
+	type ExtendedProvisioningBlocks = ConstU64<0>;
+	type OnLiquidityPoolUpdated = ();
+}
+
+impl pallet_timestamp::Config for Runtime {
+	type Moment = Moment;
+	type OnTimestampSet = ();
+	type MinimumPeriod = ConstU64<1_000>;
+	type WeightInfo = ();
+}
+
+thread_local! {
+	static IS_SHUTDOWN: RefCell<bool> = RefCell::new(false);
+}
+
+pub fn mock_shutdown() {
+	IS_SHUTDOWN.with(|v| *v.borrow_mut() = true)
+}
+
+pub struct MockEmergencyShutdown;
+impl EmergencyShutdown for MockEmergencyShutdown {
+	fn is_shutdown() -> bool {
+		IS_SHUTDOWN.with(|v| *v.borrow_mut())
+	}
+}
+
+ord_parameter_types! {
+	pub const One: AccountId = 1;
+}
+
+parameter_type_with_key! {
+	pub MinimumCollateralAmount: |_currency_id: CurrencyId| -> Balance {
+		10
+	};
+}
+
+parameter_types! {
+	pub DefaultLiquidationRatio: Ratio = Ratio::saturating_from_rational(3, 2);
+	pub DefaultDebitExchangeRate: ExchangeRate = ExchangeRate::saturating_from_rational(1, 10);
+	pub DefaultLiquidationPenalty: Rate = Rate::saturating_from_rational(10, 100);
+	pub MaxSwapSlippageCompareToOracle: Ratio = Ratio::saturating_from_rational(50, 100);
+}
+
+impl Config for Runtime {
+	type Event = Event;
+	type PriceSource = MockPriceSource;
+	type DefaultLiquidationRatio = DefaultLiquidationRatio;
+	type DefaultDebitExchangeRate = DefaultDebitExchangeRate;
+	type DefaultLiquidationPenalty = DefaultLiquidationPenalty;
+	type MinimumDebitValue = ConstU128<2>;
+	type MinimumCollateralAmount = MinimumCollateralAmount;
+	type GetStableCurrencyId = GetStableCurrencyId;
+	type CDPTreasury = CDPTreasuryModule;
+	type UpdateOrigin = EnsureSignedBy<One, AccountId>;
+	type MaxSwapSlippageCompareToOracle = MaxSwapSlippageCompareToOracle;
+	type UnsignedPriority = ConstU64<1048576>; // 1 << 20
+	type EmergencyShutdown = MockEmergencyShutdown;
+	type UnixTime = Timestamp;
+	type Currency = Currencies;
+	type DEX = DEXModule;
+	type Swap = SpecificJointsSwap<DEXModule, AlternativeSwapPathJointList>;
+	type WeightInfo = ();
+}
+
+type UncheckedExtrinsic = frame_system::mocking::MockUncheckedExtrinsic<Runtime>;
+type Block = frame_system::mocking::MockBlock<Runtime>;
+
+construct_runtime!(
+	pub enum Runtime where
+		Block = Block,
+		NodeBlock = Block,
+		UncheckedExtrinsic = UncheckedExtrinsic,
+	{
+		System: frame_system::{Pallet, Call, Storage, Config, Event<T>},
+		CDPEngineModule: cdp_engine::{Pallet, Storage, Call, Event<T>, Config, ValidateUnsigned},
+		CDPTreasuryModule: cdp_treasury::{Pallet, Storage, Call, Config, Event<T>},
+		Currencies: orml_currencies::{Pallet, Call},
+		Tokens: orml_tokens::{Pallet, Storage, Event<T>, Config<T>},
+		LoansModule: loans::{Pallet, Storage, Call, Event<T>},
+		PalletBalances: pallet_balances::{Pallet, Call, Storage, Event<T>},
+		DEXModule: dex::{Pallet, Storage, Call, Event<T>, Config<T>},
+		Timestamp: pallet_timestamp::{Pallet, Call, Storage, Inherent},
+	}
+);
+
+/// An extrinsic type used for tests.
+pub type Extrinsic = TestXt<Call, ()>;
+
+impl<LocalCall> SendTransactionTypes<LocalCall> for Runtime
+where
+	Call: From<LocalCall>,
+{
+	type OverarchingCall = Call;
+	type Extrinsic = Extrinsic;
+}
+
+pub struct ExtBuilder {
+	balances: Vec<(AccountId, CurrencyId, Balance)>,
+}
+
+impl Default for ExtBuilder {
+	fn default() -> Self {
+		Self {
+			balances: vec![
+				(ALICE, BTC, 1000),
+				(BOB, BTC, 1000),
+				(CAROL, BTC, 10000),
+				(ALICE, DOT, 1000),
+				(BOB, DOT, 1000),
+				(CAROL, DOT, 10000),
+				(CAROL, SUSD, 10000),
+			],
+		}
+	}
+}
+
+impl ExtBuilder {
+	pub fn build(self) -> sp_io::TestExternalities {
+		let mut t = frame_system::GenesisConfig::default()
+			.build_storage::<Runtime>()
+			.unwrap();
+
+		pallet_balances::GenesisConfig::<Runtime> {
+			balances: vec![(CAROL, 10000)],
+		}
+		.assimilate_storage(&mut t)
+		.unwrap();
+
+		orml_tokens::GenesisConfig::<Runtime> {
+			balances: self.balances,
+		}
+		.assimilate_storage(&mut t)
+		.unwrap();
+
+		dex::GenesisConfig::<Runtime> {
+			initial_listing_trading_pairs: vec![],
+			initial_enabled_trading_pairs: EnabledTradingPairs::get(),
+			initial_added_liquidity_pools: vec![],
+		}
+		.assimilate_storage(&mut t)
+		.unwrap();
+
+		t.into()
+	}
+
+	pub fn lots_of_accounts() -> Self {
+		let mut balances = Vec::new();
+		for i in 0..1001 {
+			let account_id: AccountId = i;
+			balances.push((account_id, BTC, 1000));
+		}
+		Self { balances }
+	}
+}
