1f1e65d5a0205bfd6e65559a39ab998ec8850068
LayNath242 <nath.selendra@gmail.com>
2022-10-15 10:08:50 +0700

    add node collation generation

---
+++ node/node-helper/collation-generation/src/error.rs
@@ -0,0 +1,33 @@
+// Copyright (C) 2021-2022 Selendra.
+// SPDX-License-Identifier: GPL-3.0-or-later WITH Classpath-exception-2.0
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+use thiserror::Error;
+
+#[derive(Debug, Error)]
+pub enum Error {
+	#[error(transparent)]
+	Subsystem(#[from] selendra_node_subsystem::SubsystemError),
+	#[error(transparent)]
+	OneshotRecv(#[from] futures::channel::oneshot::Canceled),
+	#[error(transparent)]
+	Runtime(#[from] selendra_node_subsystem::errors::RuntimeApiError),
+	#[error(transparent)]
+	Util(#[from] selendra_node_subsystem_util::Error),
+	#[error(transparent)]
+	Erasure(#[from] selendra_erasure_coding::Error),
+}
+
+pub type Result<T> = std::result::Result<T, Error>;
