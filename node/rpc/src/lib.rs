0e2a8dfaf39fd7cdeb5d182442d6f3c57a4c7dea
LayNath242 <nath.selendra@gmail.com>
2022-10-20 07:41:14 +0700

    update rpc

--- node/rpc/src/lib.rs
+++ node/rpc/src/lib.rs
@@ -19,20 +19,16 @@
 //! Selendra-specific RPCs implementation.
 
 #![warn(missing_docs)]
-#![warn(unused_crate_dependencies)]
 
 use std::sync::Arc;
 
-use primitives::v2::{AccountId, Balance, Block, BlockNumber, Hash, Nonce};
+use jsonrpsee::RpcModule;
 use sc_client_api::AuxStore;
-use sc_consensus_babe::{Config, Epoch};
-use sc_consensus_epochs::SharedEpochChanges;
-use sc_finality_grandpa::{
-	FinalityProofProvider, GrandpaJustificationStream, SharedAuthoritySet, SharedVoterState,
-};
-use sc_rpc::SubscriptionTaskExecutor;
-pub use sc_rpc_api::DenyUnsafe;
+use sc_consensus_babe::Epoch;
+use sc_finality_grandpa::FinalityProofProvider;
+pub use sc_rpc::{DenyUnsafe, SubscriptionTaskExecutor};
 use sc_transaction_pool_api::TransactionPool;
+use selendra_primitives::v2::{AccountId, Balance, Block, BlockNumber, Hash, Nonce};
 use sp_api::ProvideRuntimeApi;
 use sp_block_builder::BlockBuilder;
 use sp_blockchain::{Error as BlockChainError, HeaderBackend, HeaderMetadata};
@@ -40,48 +36,51 @@ use sp_consensus::SelectChain;
 use sp_consensus_babe::BabeApi;
 use sp_keystore::SyncCryptoStorePtr;
 
-use pallet_transaction_payment_rpc::{TransactionPayment, TransactionPaymentApiServer};
-use sc_consensus_babe_rpc::{Babe, BabeApiServer};
-use sc_finality_grandpa_rpc::{Grandpa, GrandpaApiServer};
-use sc_rpc::dev::{Dev, DevApiServer};
-use sc_sync_state_rpc::{SyncState, SyncStateApiServer};
-use substrate_frame_rpc_system::{System, SystemApiServer};
-use substrate_state_trie_migration_rpc::{StateMigration, StateMigrationApiServer};
-
 /// A type representing all RPC extensions.
-pub type RpcExtension = jsonrpsee::RpcModule<()>;
+pub type RpcExtension = RpcModule<()>;
 
 /// Extra dependencies for BABE.
 pub struct BabeDeps {
 	/// BABE protocol config.
-	pub babe_config: Config,
+	pub babe_config: sc_consensus_babe::Config,
 	/// BABE pending epoch changes.
-	pub shared_epoch_changes: SharedEpochChanges<Block, Epoch>,
+	pub shared_epoch_changes: sc_consensus_epochs::SharedEpochChanges<Block, Epoch>,
 	/// The keystore that manages the keys of the node.
 	pub keystore: SyncCryptoStorePtr,
 }
 
-/// Extra dependencies for GRANDPA
+/// Dependencies for GRANDPA
 pub struct GrandpaDeps<B> {
 	/// Voting round info.
-	pub shared_voter_state: SharedVoterState,
+	pub shared_voter_state: sc_finality_grandpa::SharedVoterState,
 	/// Authority set info.
-	pub shared_authority_set: SharedAuthoritySet<Hash, BlockNumber>,
+	pub shared_authority_set: sc_finality_grandpa::SharedAuthoritySet<Hash, BlockNumber>,
 	/// Receives notifications about justification events from Grandpa.
-	pub justification_stream: GrandpaJustificationStream<Block>,
+	pub justification_stream: sc_finality_grandpa::GrandpaJustificationStream<Block>,
 	/// Executor to drive the subscription manager in the Grandpa RPC handler.
-	pub subscription_executor: SubscriptionTaskExecutor,
+	pub subscription_executor: sc_rpc::SubscriptionTaskExecutor,
 	/// Finality proof provider.
 	pub finality_provider: Arc<FinalityProofProvider<B, Block>>,
 }
 
-/// Full client dependencies.
+use beefy_gadget::notification::{BeefyBestBlockStream, BeefySignedCommitmentStream};
+/// Dependencies for BEEFY
+pub struct BeefyDeps {
+	/// Receives notifications about signed commitment events from BEEFY.
+	pub beefy_commitment_stream: BeefySignedCommitmentStream<Block>,
+	/// Receives notifications about best block events from BEEFY.
+	pub beefy_best_block_stream: BeefyBestBlockStream<Block>,
+	/// Executor to drive the subscription manager in the BEEFY RPC handler.
+	pub subscription_executor: sc_rpc::SubscriptionTaskExecutor,
+}
+
+/// Full client dependencies
 pub struct FullDeps<C, P, SC, B> {
 	/// The client instance to use.
 	pub client: Arc<C>,
 	/// Transaction pool instance.
 	pub pool: Arc<P>,
-	/// The SelectChain Strategy
+	/// The [`SelectChain`] Strategy
 	pub select_chain: SC,
 	/// A copy of the chain spec.
 	pub chain_spec: Box<dyn sc_chain_spec::ChainSpec>,
@@ -91,34 +90,45 @@ pub struct FullDeps<C, P, SC, B> {
 	pub babe: BabeDeps,
 	/// GRANDPA specific dependencies.
 	pub grandpa: GrandpaDeps<B>,
+	/// BEEFY specific dependencies.
+	pub beefy: BeefyDeps,
 }
 
-/// Instantiate all Full RPC extensions.
+/// Instantiate all RPC extensions.
 pub fn create_full<C, P, SC, B>(
 	deps: FullDeps<C, P, SC, B>,
 	backend: Arc<B>,
 ) -> Result<RpcExtension, Box<dyn std::error::Error + Send + Sync>>
 where
 	C: ProvideRuntimeApi<Block>
-		+ sc_client_api::BlockBackend<Block>
 		+ HeaderBackend<Block>
 		+ AuxStore
 		+ HeaderMetadata<Block, Error = BlockChainError>
-		+ Sync
 		+ Send
+		+ Sync
 		+ 'static,
-	C::Api: substrate_frame_rpc_system::AccountNonceApi<Block, AccountId, Nonce>,
+	C::Api: frame_rpc_system::AccountNonceApi<Block, AccountId, Nonce>,
+	C::Api: pallet_mmr_rpc::MmrRuntimeApi<Block, <Block as sp_runtime::traits::Block>::Hash>,
 	C::Api: pallet_transaction_payment_rpc::TransactionPaymentRuntimeApi<Block, Balance>,
 	C::Api: BabeApi<Block>,
 	C::Api: BlockBuilder<Block>,
 	P: TransactionPool + Sync + Send + 'static,
 	SC: SelectChain<Block> + 'static,
 	B: sc_client_api::Backend<Block> + Send + Sync + 'static,
-	B::State: sc_client_api::backend::StateBackend<sp_runtime::traits::HashFor<Block>>,
+	B::State: sc_client_api::StateBackend<sp_runtime::traits::HashFor<Block>>,
 {
-	let mut module = RpcExtension::new(());
-	let FullDeps { client, pool, select_chain, chain_spec, deny_unsafe, babe, grandpa } = deps;
+	use beefy_gadget_rpc::{Beefy, BeefyApiServer};
+	use frame_rpc_system::{System, SystemApiServer};
+	use pallet_mmr_rpc::{Mmr, MmrApiServer};
+	use pallet_transaction_payment_rpc::{TransactionPayment, TransactionPaymentApiServer};
+	use sc_consensus_babe_rpc::{Babe, BabeApiServer};
+	use sc_finality_grandpa_rpc::{Grandpa, GrandpaApiServer};
+	use sc_sync_state_rpc::{SyncState, SyncStateApiServer};
+	use substrate_state_trie_migration_rpc::{StateMigration, StateMigrationApiServer};
 
+	let mut io = RpcModule::new(());
+	let FullDeps { client, pool, select_chain, chain_spec, deny_unsafe, babe, grandpa, beefy } =
+		deps;
 	let BabeDeps { keystore, babe_config, shared_epoch_changes } = babe;
 	let GrandpaDeps {
 		shared_voter_state,
@@ -128,10 +138,11 @@ where
 		finality_provider,
 	} = grandpa;
 
-	module.merge(System::new(client.clone(), pool, deny_unsafe).into_rpc())?;
-	module.merge(TransactionPayment::new(client.clone()).into_rpc())?;
-
-	module.merge(
+	io.merge(StateMigration::new(client.clone(), backend, deny_unsafe).into_rpc())?;
+	io.merge(System::new(client.clone(), pool.clone(), deny_unsafe).into_rpc())?;
+	io.merge(TransactionPayment::new(client.clone()).into_rpc())?;
+	io.merge(Mmr::new(client.clone()).into_rpc())?;
+	io.merge(
 		Babe::new(
 			client.clone(),
 			shared_epoch_changes.clone(),
@@ -142,7 +153,7 @@ where
 		)
 		.into_rpc(),
 	)?;
-	module.merge(
+	io.merge(
 		Grandpa::new(
 			subscription_executor,
 			shared_authority_set.clone(),
@@ -152,14 +163,18 @@ where
 		)
 		.into_rpc(),
 	)?;
-
-	module.merge(
-		SyncState::new(chain_spec, client.clone(), shared_authority_set, shared_epoch_changes)?
-			.into_rpc(),
+	io.merge(
+		SyncState::new(chain_spec, client, shared_authority_set, shared_epoch_changes)?.into_rpc(),
 	)?;
 
-	module.merge(StateMigration::new(client.clone(), backend, deny_unsafe).into_rpc())?;
-	module.merge(Dev::new(client, deny_unsafe).into_rpc())?;
+	io.merge(
+		Beefy::<Block>::new(
+			beefy.beefy_commitment_stream,
+			beefy.beefy_best_block_stream,
+			beefy.subscription_executor,
+		)?
+		.into_rpc(),
+	)?;
 
-	Ok(module)
+	Ok(io)
 }
