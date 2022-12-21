de55d512cce528b958fd6dc1862978c63edf4525
LayNath242 <nath.selendra@gmail.com>
2022-12-21 15:53:16 +0700

    Introduce RPC Worker

--- tests/test-helpers/forests/service/src/lib.rs
+++ tests/test-helpers/forests/service/src/lib.rs
@@ -37,7 +37,7 @@ use forests_client_service::{
 use forests_primitives_core::ParaId;
 use forests_relay_chain_inprocess_interface::RelayChainInProcessInterface;
 use forests_relay_chain_interface::{RelayChainError, RelayChainInterface, RelayChainResult};
-use forests_relay_chain_rpc_interface::RelayChainRPCInterface;
+use forests_relay_chain_rpc_interface::{create_client_and_start_worker, RelayChainRpcInterface};
 use forests_test_runtime::{Hash, Header, NodeBlock as Block, RuntimeApi};
 use parking_lot::Mutex;
 
@@ -182,7 +182,8 @@ async fn build_relay_chain_interface(
 	task_manager: &mut TaskManager,
 ) -> RelayChainResult<Arc<dyn RelayChainInterface + 'static>> {
 	if let Some(relay_chain_url) = collator_options.relay_chain_rpc_url {
-		return Ok(Arc::new(RelayChainRPCInterface::new(relay_chain_url).await?) as Arc<_>)
+		let client = create_client_and_start_worker(relay_chain_url, task_manager).await?;
+		return Ok(Arc::new(RelayChainRpcInterface::new(client)) as Arc<_>)
 	}
 
 	let relay_chain_full_node = test_service::new_full(
