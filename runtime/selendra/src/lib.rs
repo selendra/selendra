bac13907a26347b79174abf669294094d6ee46f2
LayNath242 <nath.selendra@gmail.com>
2023-01-26 13:43:35 +0700

    mirgate pallet_nomination_pools

--- runtime/selendra/src/lib.rs
+++ runtime/selendra/src/lib.rs
@@ -1162,7 +1162,7 @@ pub type Executive = frame_executive::Executive<
 	frame_system::ChainContext<Runtime>,
 	Runtime,
 	AllPalletsWithSystem,
-	(),
+	pallet_nomination_pools::migration::v3::MigrateToV3<Runtime>,
 >;
 /// The payload being signed in transactions.
 pub type SignedPayload = generic::SignedPayload<Call, SignedExtra>;
