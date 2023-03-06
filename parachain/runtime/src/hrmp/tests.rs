1cf6f93ec5bad8ad16451f0454653de023d2dc95
LayNath242 <nath.selendra@gmail.com>
2023-03-06 11:31:07 +0700

    Replace parachain/parathread by enum

--- parachain/runtime/src/hrmp/tests.rs
+++ parachain/runtime/src/hrmp/tests.rs
@@ -1,23 +1,26 @@
-// Copyright (C) 2021-2022 Selendra.
-// SPDX-License-Identifier: GPL-3.0-or-later WITH Classpath-exception-2.0
+// Copyright 2020 Parity Technologies (UK) Ltd.
+// This file is part of Polkadot.
 
-// This program is free software: you can redistribute it and/or modify
+// Polkadot is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
 
-// This program is distributed in the hope that it will be useful,
+// Polkadot is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
 
 // You should have received a copy of the GNU General Public License
-// along with this program. If not, see <https://www.gnu.org/licenses/>.
+// along with Polkadot.  If not, see <http://www.gnu.org/licenses/>.
 
 use super::*;
-use crate::mock::{
-	new_test_ext, Configuration, Hrmp, MockGenesisConfig, Paras, ParasShared,
-	RuntimeEvent as MockEvent, RuntimeOrigin, System, Test,
+use crate::{
+	mock::{
+		new_test_ext, Configuration, Hrmp, MockGenesisConfig, Paras, ParasShared,
+		RuntimeEvent as MockEvent, RuntimeOrigin, System, Test,
+	},
+	paras::ParaKind,
 };
 use frame_support::{assert_noop, assert_ok, traits::Currency as _};
 use primitives::v2::BlockNumber;
@@ -130,7 +133,7 @@ fn register_parachain_with_balance(id: ParaId, balance: Balance) {
 	assert_ok!(Paras::schedule_para_initialize(
 		id,
 		crate::paras::ParaGenesisArgs {
-			parachain: true,
+			para_kind: ParaKind::Parachain,
 			genesis_head: vec![1].into(),
 			validation_code: vec![1].into(),
 		},
