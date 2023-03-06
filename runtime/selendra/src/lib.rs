c15b34647963c6e36c9065bdb34de314256d03c5
LayNath242 <nath.selendra@gmail.com>
2023-03-06 13:09:15 +0700

    bump selendra spec

--- runtime/selendra/src/lib.rs
+++ runtime/selendra/src/lib.rs
@@ -123,13 +123,13 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("selendra"),
 	impl_name: create_runtime_str!("selendra"),
 	authoring_version: 1,
-	spec_version: 2016,
+	spec_version: 2017,
 	impl_version: 0,
 	#[cfg(not(feature = "disable-runtime-api"))]
 	apis: RUNTIME_API_VERSIONS,
 	#[cfg(feature = "disable-runtime-api")]
 	apis: sp_version::create_apis_vec![[]],
-	transaction_version: 2,
+	transaction_version: 3,
 	state_version: 0,
 };
 
