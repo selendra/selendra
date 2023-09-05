2e8d2b6fe3347dcb9ae40e0156d1423ce9f29f70
nath.selendra <nath.selendra@gmail.com>
2023-09-05 14:31:02 +0700

    inscrease spec_version

--- runtime/selendra/src/lib.rs
+++ runtime/selendra/src/lib.rs
@@ -138,7 +138,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("selendra"),
 	impl_name: create_runtime_str!("selendra-testnet"),
 	authoring_version: 1,
-	spec_version: 10030,
+	spec_version: 10040,
 	impl_version: 0,
 	#[cfg(not(feature = "disable-runtime-api"))]
 	apis: RUNTIME_API_VERSIONS,
