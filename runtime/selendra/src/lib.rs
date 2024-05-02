ecdbaf021d29f04cc4ed010b4c54683ac34a51f7
Nath242 <nath.selendra@gmail.com>
2024-05-02 20:33:59 +0700

    add runtime migration

--- runtime/selendra/src/lib.rs
+++ runtime/selendra/src/lib.rs
@@ -92,8 +92,8 @@ use selendra_runtime_constants::{currency::*, fee::*, time::*};
 
 // Weights used in the runtime.
 mod weights;
-
 mod bag_thresholds;
+mod migration;
 
 pub mod evm;
 pub mod governance;
@@ -976,19 +976,6 @@ construct_runtime! {
 	}
 }
 
-/// All migrations that will run on the next runtime upgrade.
-///
-/// This contains the combined migrations of the last 10 releases. It allows to skip runtime
-/// upgrades in case governance decides to do so. THE ORDER IS IMPORTANT.
-pub type Migrations = (migrations::Unreleased,);
-
-/// The runtime migrations per release.
-#[allow(deprecated, missing_docs)]
-pub mod migrations {
-	/// Unreleased migrations. Add new ones here:
-	pub type Unreleased = ();
-}
-
 /// The address format for describing accounts.
 pub type Address = sp_runtime::MultiAddress<AccountId, ()>;
 /// Block header type as expected by this runtime.
@@ -1025,7 +1012,7 @@ pub type Executive = frame_executive::Executive<
 	frame_system::ChainContext<Runtime>,
 	Runtime,
 	AllPalletsWithSystem,
-	Migrations,
+	migration::Migrations,
 >;
 
 /// The payload being signed in transactions.
