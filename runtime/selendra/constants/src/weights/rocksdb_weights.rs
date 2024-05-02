4f686f41636fab82482ad0d3d3de42943b590f16
Nath242 <nath.selendra@gmail.com>
2024-05-02 20:10:03 +0700

    remove rust stable warming

--- runtime/selendra/constants/src/weights/rocksdb_weights.rs
+++ runtime/selendra/constants/src/weights/rocksdb_weights.rs
@@ -32,7 +32,7 @@
 //   --mul=1.1
 //   --weight-path=runtime/selendra/constants/src/weights/
 
-/// Storage DB weights for the `Selendra` runtime and `RocksDb`.
+// Storage DB weights for the `Selendra` runtime and `RocksDb`.
 
 pub mod constants {
 	use frame_support::{
@@ -41,37 +41,37 @@ pub mod constants {
 	};
 
 	parameter_types! {
-		/// By default, Substrate uses `RocksDB`, so this will be the weight used throughout
-		/// the runtime.
+		// By default, Substrate uses `RocksDB`, so this will be the weight used throughout
+		// the runtime.
 		pub const RocksDbWeight: RuntimeDbWeight = RuntimeDbWeight {
-			/// Time to read one storage item.
-			/// Calculated by multiplying the *Average* of all values with `1.1` and adding `0`.
-			///
-			/// Stats [NS]:
-			///   Min, Max: 5_015, 1_441_022
-			///   Average:  18_635
-			///   Median:   17_795
-			///   Std-Dev:  4829.75
-			///
-			/// Percentiles [NS]:
-			///   99th: 32_074
-			///   95th: 26_658
-			///   75th: 19_363
+			// Time to read one storage item.
+			// Calculated by multiplying the *Average* of all values with `1.1` and adding `0`.
+			//
+			// Stats [NS]:
+			//   Min, Max: 5_015, 1_441_022
+			//   Average:  18_635
+			//   Median:   17_795
+			//   Std-Dev:  4829.75
+			//
+			// Percentiles [NS]:
+			//   99th: 32_074
+			//   95th: 26_658
+			//   75th: 19_363
 			read: 20_499 * constants::WEIGHT_REF_TIME_PER_NANOS,
 
-			/// Time to write one storage item.
-			/// Calculated by multiplying the *Average* of all values with `1.1` and adding `0`.
-			///
-			/// Stats [NS]:
-			///   Min, Max: 16_368, 34_500_937
-			///   Average:  75_882
-			///   Median:   74_236
-			///   Std-Dev:  64706.41
-			///
-			/// Percentiles [NS]:
-			///   99th: 111_151
-			///   95th: 92_666
-			///   75th: 80_297
+			// Time to write one storage item.
+			// Calculated by multiplying the *Average* of all values with `1.1` and adding `0`.
+			//
+			// Stats [NS]:
+			//   Min, Max: 16_368, 34_500_937
+			//   Average:  75_882
+			//   Median:   74_236
+			//   Std-Dev:  64706.41
+			//
+			// Percentiles [NS]:
+			//   99th: 111_151
+			//   95th: 92_666
+			//   75th: 80_297
 			write: 83_471 * constants::WEIGHT_REF_TIME_PER_NANOS,
 		};
 	}
@@ -81,7 +81,7 @@ pub mod constants {
 		use super::constants::RocksDbWeight as W;
 		use frame_support::weights::constants;
 
-		/// Checks that all weights exist and have sane values.
+		// Checks that all weights exist and have sane values.
 		// NOTE: If this test fails but you are sure that the generated values are fine,
 		// you can delete it.
 		#[test]
