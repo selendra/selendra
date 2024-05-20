e6a34cc1f880f9b4408dd7ac27dfc7fcbfaefd63
Nath242 <nath.selendra@gmail.com>
2024-05-20 09:47:21 +0700

    update genesisi assets

--- node/src/starknet/constants.rs
+++ node/src/starknet/constants.rs
@@ -1,5 +1,5 @@
 pub const DEFAULT_CONFIGS_URL: &str =
-    "https://raw.githubusercontent.com/keep-starknet-strange/madara/main/configs/index.json";
+    "https://raw.githubusercontent.com/selendra/selendra/starknet/config/index.json";
 
 pub const DEV_CHAIN_ID: &str = "dev";
 
