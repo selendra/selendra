76ee8b597e5dc631c02674bdf904d17c637097ae
Nath242 <nath.selendra@gmail.com>
2024-08-05 07:02:24 +0700

    bump version

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -63,7 +63,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("selendra"),
 	impl_name: create_runtime_str!("selendra"),
 	authoring_version: 1,
-	spec_version: 11000,
+	spec_version: 20000,
 	impl_version: 1,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
