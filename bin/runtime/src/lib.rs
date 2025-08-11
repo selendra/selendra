ee96b09de82fa294b8a51323f6428e75ffba73be
LayNath242 <nath.selendra@gamail.com>
2025-08-11 08:07:48 +0700

    update evm  dynamic fee

--- bin/runtime/src/lib.rs
+++ bin/runtime/src/lib.rs
@@ -89,7 +89,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("selendra"),
 	impl_name: create_runtime_str!("selendra"),
 	authoring_version: 1,
-	spec_version: 20002,
+	spec_version: 20003,
 	impl_version: 1,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
