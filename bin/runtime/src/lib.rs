35c8bdeb90c3881063972fcecf1bfd61d1895536
Nath242 <nath.selendra@gmail.com>
2025-10-27 13:55:51 +0700

    upgrade spec_version

--- bin/runtime/src/lib.rs
+++ bin/runtime/src/lib.rs
@@ -89,7 +89,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("selendra"),
 	impl_name: create_runtime_str!("selendra"),
 	authoring_version: 1,
-	spec_version: 20004,
+	spec_version: 20005,
 	impl_version: 1,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
